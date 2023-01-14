ff43484049acd50f7f9750ad2f3c5786b9d0b320
Sanyam07 <agrawalsanyam07@gmail.com>
2023-01-14 20:26:09 +0530

    Functionality implemented

---
+++ index.js
@@ -0,0 +1,30 @@
+window.addEventListener('load',()=>{
+    const $newTaskForm = document.querySelector('#newTaskForm')
+    const $newTaskInput = document.querySelector('#newTaskInput')
+    const $tasks = document.querySelector('#tasks')
+    const $taskTemplate = document.querySelector('#task-template').innerHTML
+    
+    $newTaskForm.addEventListener('submit',(e)=>{
+        e.preventDefault()
+        const task = $newTaskInput.value
+        const html = Mustache.render($taskTemplate,{
+            task,
+            taskId: `${task}task`,
+            remId: `${task}rem`
+        })
+        $tasks.insertAdjacentHTML('beforeend', html)
+        var scriptElm = document.createElement('script');
+        scriptElm.setAttribute('id',`${task}script`)
+        var inlineCode = document.createTextNode(`
+            document.querySelector('#${task}rem').addEventListener('click',(e)=>{
+                e.preventDefault()
+                const html = document.querySelector('#${task}task')
+                html.remove()
+                const script = document.querySelector('#${task}script')
+                script.remove()
+            })
+        `);
+        scriptElm.appendChild(inlineCode); 
+        document.body.appendChild(scriptElm);
+    })
+})
